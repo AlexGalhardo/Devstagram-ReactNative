abefc4728ec27c83a284745de92558eeed7f243b
Tiago L. Camargo <tiagolc@live.com>
2019-04-12 16:14:23 -0300

    Tela de cadastro criada

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { TextInput, TouchableHighlight, ImageBackground, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { checkLogin } from '../actions/AuthActions'
 
@@ -14,11 +14,27 @@ export class Login extends Component {
       this.state = {}
    }
 
+   signUpAction = () => {
+      this.props.navigation.navigate('SignUp');
+   }
+
    render() {
       return (
-         <View style={styles.container}>
-            <Text>Pagina de Login</Text>
-         </View>
+         <ImageBackground source={require('../assets/bg.png')} style={styles.container}>
+            <Text style={styles.logo}>PhotoSphere</Text>
+
+            <TextInput style={styles.input} placeholder="Digite seu e-mail" placeholderTextColor="#ffffff" underlineColorAndroid="transparent"/>
+            <TextInput style={styles.input} placeholder="Digite sua senha" placeholderTextColor="#ffffff" secureTextEntry={true} underlineColorAndroid="transparent"/>
+            
+            <TouchableHighlight onPress={() => { }} underlayColor="#307eaf" style={styles.actionButton}>
+               <Text style={styles.actionButtonText}>Fazer Login</Text>
+            </TouchableHighlight>
+
+            <TouchableHighlight onPress={this.signUpAction} underlayColor="transparent" style={styles.signButton}>
+               <Text style={styles.signButtonText}>Ainda não tem cadastro? Clique aqui</Text>
+            </TouchableHighlight>
+         
+         </ImageBackground>
       )
    }
 }
@@ -27,7 +43,48 @@ const styles = StyleSheet.create({
    container: {
       flex: 1,
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'center',
+      resizeMode: 'contain'
+   },
+   logo:{
+      fontSize: 45,
+      color: '#ffffff',
+      marginBottom: 30
+   },
+   input:{
+      width: "90%",
+      height: 50,
+      backgroundColor: 'rgba(255, 255, 255, 0.15)',
+      borderRadius: 10,
+      color: '#ffffff',
+      fontSize:18,
+      marginBottom: 12,
+   },
+   actionButton:{
+      width: "40%",
+      height: 40,
+      backgroundColor: 'transparent',
+      borderRadius: 5,
+      borderWidth: 1,
+      borderColor: '#ffffff',
+      justifyContent: 'center',
+      alignItems: 'center',
+   },
+   actionButtonText:{
+      color:'#ffffff',
+      fontSize: 16
+   },
+   signButton:{
+      width: "90%",
+      height: 50,
+      backgroundColor: 'transparent',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 20
+   },
+   signButtonText:{
+      color: '#ffffff',
+      fontSize: 14
    }
 });
 
